07c1cf0579ac69d645c6bbce4b2a2f8285903f30
minako <minako>
2013-12-27 15:55:10 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class TheForm
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox
+			// 
+			this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox.Location = new System.Drawing.Point(0, 0);
+			this.textBox.Multiline = true;
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(250, 166);
+			this.textBox.TabIndex = 0;
+			// 
+			// TheForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(250, 166);
+			this.Controls.Add(this.textBox);
+			this.Font = new System.Drawing.Font("メイリオ", 12F);
+			this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "TheForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "システムデーモン";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TheForm_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox;
+
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	public partial class TheForm : Form
+	{
+		static TheForm self;
+		const int UDP_REBOOT_PORT = 37501;
+		UdpClient udp_recv;
+		static Thread thread;
+		static bool exit_thread = false;
+
+		public TheForm()
+		{
+			self = this;
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			udp_recv = new UdpClient(UDP_REBOOT_PORT);
+			thread = new Thread(ReceiveProc);
+			thread.Start();
+		}
+
+		public static void ReceiveProc()
+		{
+			while (!exit_thread)
+			{
+				self.ReceiveThreadProc();
+				Thread.Sleep(1);
+			}
+		}
+
+		public void ReceiveThreadProc()
+		{
+			var enc = System.Text.Encoding.UTF8;
+
+			if (udp_recv.Available > 0)
+			{
+				// Receive udp data
+				IPEndPoint remoteEP = null;
+				byte[] rcvBytes = udp_recv.Receive(ref remoteEP);
+				if (rcvBytes.Length > 0)
+				{
+					Debug.Print("[udp recv] " + rcvBytes.Length.ToString() + " bytes received");
+					string rcvMsg = enc.GetString(rcvBytes);
+					rcvMsg = rcvMsg.Replace('\t', ' ').Replace('\n', ' ').Trim();
+					string[] cmds = rcvMsg.Split('|');
+					if (cmds.Length >= 2)
+					{
+						DoCommand(cmds[0], cmds[1]);
+					}
+					else if (cmds.Length>=1)
+					{
+						DoCommand(cmds[0], "");
+					}
+				}
+			}
+		}
+
+		void DoCommand(string cmd, string arg)
+		{
+			if (cmd == "bye")
+			{
+				Application.Exit();
+			}
+			else
+			{
+				Debug.Print("{0} {1}", cmd, arg);
+				var psi = new ProcessStartInfo();
+				psi.FileName = cmd;
+				psi.Arguments = arg;
+				psi.CreateNoWindow = false;
+				var p = Process.Start(psi);
+			}
+		}
+
+		private void TheForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			exit_thread = true;
+		}
+	}
+}
